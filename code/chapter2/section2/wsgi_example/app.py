367a1dadad3e6b7d072843a21ad85f5482656983
RainingHeart <lzy_2318@163.com>
2019-04-22 22:12:31 +0800

    student_sys admin

--- code/chapter2/section2/wsgi_example/app.py
+++ code/chapter2/section2/wsgi_example/app.py
@@ -7,3 +7,27 @@ def simple_app(environ, start_response):
     response_headers = [('Content-Type', 'text/plain')]
     start_response(status, response_headers)
     return [b'Hello world! -by the5fire \n']
+
+
+class AppClass(object):
+    status = '200 OK'
+    response_headers = [('Content-Type', 'text/plain')]
+
+    def __call__(self, environ, start_response):
+        print(environ, start_response)
+        start_response(self.status, self.response_headers)
+        return [b'Hello Appclass.__call__\n']
+
+
+class AppClassIter(object):
+    status = '200 OK'
+    response_headers = [('Content-Type', 'text/plain')]
+
+    def __init__(self, environ, start_response):
+        self.environ = environ
+        self.start_response = start_response
+
+    def __iter__(self):
+        self.start_response(self.status, self.response_headers)
+        yield b'Hello AppClassIter\n'
+
